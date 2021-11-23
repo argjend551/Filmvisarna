3dccb56a2a407eb0876d829557d3de20d0583e0d
argjend551 <argjend551@hotmail.com>
2021-11-23 12:58:29 +0100

    Nav bar

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const path = require("path");
-
 // Create a webserver
 const app = express();
 
@@ -9,17 +7,9 @@ const app = express();
 const jsonflex = require('jsonflex')({ jsonDir: '/frontend/json' });
 app.use(jsonflex);
 
- 
 // Tell the web server to serve files from the frontend folder
 app.use(express.static('frontend'));
 
-
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
-//Any route entered will be sent to the file index.html
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
-});
-
-
 // Start the web server at port 3000
 app.listen(3000, () => console.log('Listening on port 3000'));
+
