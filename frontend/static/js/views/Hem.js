b27a60d4c82a94891d0eaab25ea4318b0629f35b
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2021-11-24 15:36:54 +0100

    commiting before pull

--- frontend/static/js/views/Hem.js
+++ frontend/static/js/views/Hem.js
@@ -13,22 +13,22 @@ export default class extends AbstractView {
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
   <div class="carousel-inner">
     <div class="carousel-item active">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/Xm12NSa8jsM?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Xm12NSa8jsM?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
     </div>
     <div class="carousel-item">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/aeVkbNka9HM?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/aeVkbNka9HM?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
     </div>
     <div class="carousel-item" >
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/AkdXuDAP2Ts?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/AkdXuDAP2Ts?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
     </div>
     <div class="carousel-item" >
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/BIhNsAtPbPI?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/BIhNsAtPbPI?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
     </div>
     <div class="carousel-item" >
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Sy8nPI85Ih4?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Sy8nPI85Ih4?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
     </div>
     <div class="carousel-item" >
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/yEmcEuS6xm4?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/yEmcEuS6xm4?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
