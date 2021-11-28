fd3ce812e1857a416e8693a00e864fee23e069bf
argjend551 <argjend551@hotmail.com>
2021-11-28 16:27:06 +0100

    Har lagt in kod så när man klickar på en film i startsidan så visas det upp en tabell på alla tider och om man klickar så får man upp salongen

--- frontend/js/filmer.js
+++ frontend/js/filmer.js
@@ -1,6 +1,7 @@
 //Content for the films view
 function filmer(id) {
   let film = data.filmer.find(x => x.id === id);
+ 
   $('main').html(`
   <div id="filmContainer">
   <p><iframe width="560" height="315" src="https://www.youtube.com/embed/${film.youtubeTrailers}?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe></p>
@@ -10,6 +11,59 @@ Längd: ${film.length}<br> Genre: ${film.genre} <br> Åldersgräns: ${film.ageRe
      Regissör: ${film.director}<br> Skådespelare: ${film.actors} <br> Handling: ${film.description}
      Recensioner: ${film.reviews[0].source}<br>
       ${film.reviews[0].quote}<br> ${film.reviews[0].stars}<br> ${film.reviews[0].max}</div>
+
+
+
+   <div id="span3">
+
+
+
+
+
+     <table class="table">
+          <thead>
+               <tr>
+                    <th scope="col">Film</th>
+                    <th scope="col">Datum</th>
+                    <th scope="col">Salong</th>
+
+               </tr>
+          </thead>
+          <tbody>
+               <th class="film"></th>
+               <th class="datum"></th>
+               <th class="salong"></th>
+
+          </tbody>
+     </table>
+
+
+</div>
+
+
+ <div id="cinema">
+          <div class="screen"></div>
+
+          <div class="col-12 col-sm-6 col-lg-3">
+               <div class="row1"></div>
+               <div class="row2"></div>
+               <div class="row3"></div>
+               <div class="row4"></div>
+               <div class="row5"></div>
+               <div class="row6"></div>
+               <div class="row7"></div>
+               <div class="row8"></div>
+          </div>
+          <button class="book">Book</button>
+
+     </div>
+
+
+
+
+
     `);
+  console.log(film.title);
+  findByfilm('shows', film.title);
 
 }
--- frontend/js/filter.js
+++ frontend/js/filter.js
@@ -78,7 +78,6 @@ function filterfilm(a) {
                $(".film").empty();
                $(".datum").empty();
                $(".salong").empty();
-               starter();
 
           }
 
