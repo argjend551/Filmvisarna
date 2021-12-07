d6d0568916f4b7758d3c58a1042895eb25e17593
denjennie <denjennie@live.se>
2021-12-07 12:51:00 +0100

    Fixat så slidern rullar på automatiskt

--- frontend/js/hem.js
+++ frontend/js/hem.js
@@ -1,41 +1,44 @@
-
 // Content for the start/hem page
 function hem() {
   $('main').html(`
-<div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel" data-interval="1000">
+  
+ <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" >
   <div class="carousel-inner">
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="Filmer-images/bad-boys-2.jpg" class="d-block w-100" alt="bad-boys-2.jpg">
+    </div>
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="Filmer-images/10-things-i-hate-about-you-2.png" class="d-block w-100" alt="10-things-i-hate-about-you-2.png">
+    </div>
     <div class="carousel-item active" data-bs-interval="4000">
-    <img src="Filmer-images/dead-pool-2.jpg" class="d-block w-100" max-width="460" height="auto" alt="dead-pool-2.jpg">
+      <img src="Filmer-images/dead-pool-2.jpg" class="d-block w-100" alt="dead-pool-2.jpg">
     </div>
-    <div class="carousel-item">
-      <img src="Filmer-images/bad-boys-2.jpg" class="d-block w-100" max-width="460" height="auto" alt="bad-boys-2.jpg">
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="Filmer-images/No-time-to-die-2.jpg" class="d-block w-100" alt="No-time-to-die-2.jpg">
     </div>
-    <div class="carousel-item">
-         <img src="Filmer-images/10-things-i-hate-about-you-2.png" class="d-block w-100" max-width="460" height="auto"  alt="Filmer-images/10-things-i-hate-about-you-2.png">
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="Filmer-images/up-2.jpg" class="d-block w-100" alt="up-2.jpg">
     </div>
-     <div class="carousel-item">
-      <img src="Filmer-images/No-time-to-die-2.jpg" class="d-block w-100" max-width="460" height="auto"  alt="No-time-to-die-2.jpg">
-    </div>
-     <div class="carousel-item">
-      <img src="Filmer-images/up-2.jpg" class="d-block w-100" max-width=" 460" height="auto"  alt="up-2.jpg">
-    </div>
-    <div class="carousel-item">
-      <img src="Filmer-images/white-chicks-2.jpg" class="d-block w-100" max-width=" 460" height="auto"  alt="white-chicks-2.jpg">
+    <div class="carousel-item" data-bs-interval="4000">
+      <img src="Filmer-images/white-chicks-2.jpg" class="d-block w-100" alt="white-chicks-2.jpg">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-
- 
 `);
-  let html = '';
+  $('.carousel').carousel({
+    interval: 4000,
+    cycle: true
+  });
+
+
   var newdiv = document.createElement('div');
   newdiv.className = 'row';
 
@@ -44,14 +47,8 @@ function hem() {
     let div = document.createElement("div");
     div.className = 'col-md-4 col-xs-4';
     newdiv.appendChild(div);
-    div.innerHTML = `<div class = "hem-poster-title"><p><a href="#film-${data.filmer[i].id}"><img class="poster" src="${data.filmer[i].images}"></img></a></p><h2 id ='title'><br>  ${data.filmer[i].title} <br></h2> </div>`
+    div.innerHTML = `<p><a href="#film-${data.filmer[i].id}"><img class="poster" src="${data.filmer[i].images}"></img></a></p><h2 id ='title'><br>  ${data.filmer[i].title} <br></h2>`
   }
-  //console.log('html är:' + html.innerHTML);
+  //console.log('html Ã¤r:' + html.innerHTML);
   $('main').append(newdiv);
 }
-loadCarousel()
-async function loadCarousel() {
-  let myCarousel = await $('#carouselExampleInterval');
-  console.log(myCarousel)
-}
-
