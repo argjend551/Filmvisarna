93429329340d32ddabebafd9c220fd4cfc21a204
AliLiloo <alililo95@outlook.com>
2021-12-16 15:42:13 +0100

    >ALi & Jennie har fixat designen på filmsidan, gjort hela responsiv med trailer, bilder och text

--- frontend/js/filmer.js
+++ frontend/js/filmer.js
@@ -1,68 +1,103 @@
 //Content for the films view
-// fetch all informations (data) about the film when you click on a specific film with help of id 
-
-
-
 function filmer(id) {
      let film = data.filmer.find(x => x.id === id);
 
-     $('main').html(`
-  <div id="filmContainer">
-  <p><iframe width="560" height="315" src="https://www.youtube.com/embed/${film.youtubeTrailers}?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="d-block w-100"></iframe></p>
-   <img class='post' img style= "height: 500px"src="${film.images}"></img> 
-   </div>
-   <div class="filmText">
-   <h3> Titel: ${film.title} </h3>  
-   <p>Produktionsland: ${film.productionCountries} <br> Produktionsår: ${film.productionYear} <br>
-      Längd: ${film.length}<br> Genre: ${film.genre} <br> Åldersgräns: ${film.ageRestrictions}
-  <br> Distributör: ${film.distributor}<br> Språk: ${film.language}<br> Textning: ${film.subtitles} <br>
-     Regissör: ${film.director}<br> Skådespelare: ${film.actors} <br> Handling: ${film.description} 
-     Recensioner: ${film.reviews[0].source} <br> ${film.reviews[0].quote} </p>
-    <h5> Ratning: ${film.reviews[0].stars}  / ${film.reviews[0].max}<h5></div>
+     $('main').html(/*html*/`
+		<div class="row camera-bg">
+			<div class="col-12">
+				<iframe class="youtubeTrailers" content="width=device-width, initial-scale=1.0" src="https://www.youtube.com/embed/${film.youtubeTrailers}
+														" title="YouTube video" allowfullscreen></iframe>
+			</div>
+		</div>
 
 
-   <div id="span3">
+		<div class="row mt-5">
+
+  		<div class="offset-1 col-10 offset-sm-2 col-sm-8 offset-md-2 col-md-4">
+				<div class="film-images-fluid">
+					<img src="${film.images}" class="img-fluid" style="border-radius: 5%" alt="filmer">
+				</div>
+			</div>
+
+			<div class="offset-1 col-10 offset-sm-2 col-sm-8 col-md-4 mt-5 mt-md-0 offset-md-0">
+				<div class="text-white">
+					<h3> Titel: ${film.title} </h3>
+					<p>Produktionsland: ${film.productionCountries} <br> Produktionsår: ${film.productionYear} <br>
+						Längd: ${film.length}<br> Genre: ${film.genre} <br> Åldersgräns: ${film.ageRestrictions}
+						<br> Distributör: ${film.distributor}<br> Språk: ${film.language}<br> Textning: ${film.subtitles} <br>
+						Regissör: ${film.director}<br> Skådespelare: ${film.actors} <br>
+						Handling: ${film.description}
+						Recensioner: ${film.reviews[0].source} <br> ${film.reviews[0].quote}
+					</p>
+					<h5> Ratning: ${film.reviews[0].stars} / ${film.reviews[0].max}<h5>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="col-sm">
+			
+		</div>
+		</div>
+		</div>
+
+		</div>
+		<div id="span3">
 
 
-     <table class="table table-hover">
-          <thead>
-               <tr>
-                      <th scope="col">Film</th>
-                    <th scope="col">Datum</th>
-                     <th scope="col">Tid</th>
-                    <th scope="col">Salong</th>
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th scope="col">Film</th>
+						<th scope="col">Datum</th>
+						<th scope="col">Tid</th>
+						<th scope="col">Salong</th>
 
-               </tr>
-          </thead>
-          <tbody class="table">
+					</tr>
+				</thead>
+				<tbody class="table">
 
-          </tbody>
-     </table>
-
-
-</div>
+				</tbody>
+			</table>
 
 
- <div class="cinema" >
-  <div class="span">
-  <div class="screen">
- 
- </div>
- </div>
-<div class="col-12" >
-          <div class="all-seats">
-
-          </div>
-                   <button type="button" disabled class="btn btn-success btn3">Boka</button>
-
-</div>
-     </div>
+		</div>
 
 
+		<div class="cinema">
+			<div class="col-12">
+				<div class="all-seats">
 
+				</div>
+				<button type="button" disabled class="btn btn-success">Boka</button>
 
-    `);
-     window.scrollTo(0, 0);
+			</div>
+		</div>
+
+		</div>
+  `);
+     console.log(film.title);
      findByfilm('shows', film.title);
 
+     cameraImageSetHeight();
 }
+
+
+// Set proportional height on the camera image
+function cameraImageSetHeight() {
+     // camera image element
+     let ci = $('.camera-bg');
+     ci.height(''); // reset before reading width
+     let width = ci.width();
+     let height = width * 768 / 1366; // proportional height
+     ci.height(height);
+     // trailer set height proportional to width
+     let yt = $('.youtubeTrailers');
+     yt.height();
+     let ywidth = yt.width();
+     let yheight = ywidth * 9 / 16;
+     yt.height(yheight);
+}
+
+
+$(window).resize(cameraImageSetHeight);
